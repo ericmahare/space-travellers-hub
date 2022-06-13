12341987ad1d8bfdecfc503f0ad568ac1b27c209
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-13 11:29:44 +0100

    Added the Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/rocket/Navbar';
+import Rocket from './pages/Rocket';
+import Mission from './pages/Mission';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Rocket />} />
+          <Route path="mission" element={<Mission />} />
+          <Route path="profile" element={<Profile />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
---
+++ src/components/rocket/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../../assets/solar-logo.png';
+import '../../scss/Navbar.scss'
+
+function Navbar() {
+    return (
+      <header>
+        <div className='navbar d-flex'>
+          <div className="navTitle d-flex">
+            <img src ={logo} alt="logo" className="logo" />
+            <h1>Space Travelers' Hub</h1>
+          </div>
+
+          <div className="navMenu d-flex">
+            <NavLink to="/" className="rocket">
+              <span>Rockets</span>
+            </NavLink>
+
+            <NavLink to="mission" className="rocket">
+              <span>Mission</span>
+            </NavLink>
+            {' '}
+            |
+            <NavLink to="profile" className="rocket">
+              <span>My Profile</span>
+            </NavLink>
+          </div>
+        </div>
+      </header>
+    );
+}
+  
+export default Navbar;
