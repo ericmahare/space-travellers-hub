1c185e06c931bb8ce6302805311f108ab98e6f8c
ericmahare <erikmahare@gmail.com>
2022-06-13 18:40:52 +0300

    Add rocket hard coded list

--- src/components/rocket/RocketList.js
+++ src/components/rocket/RocketList.js
@@ -1,21 +1,29 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { FetchRockets } from '../../redux/rocket/rocketSlice';
+import React from 'react';
 import RocketCard from './RocketCard';
 import '../../scss/RocketList.scss';
-
+import rocketImg from '../../images/rocket.jpg';
 const Rockets = () => {
-  const rocketsinfo = useSelector((state) => state.rocketsReducer);
+  const rockets = [
+    {
+      rocket_id:1,
+      rocket_name:'Rocket 1',
+      rocket_img: rocketImg,
+      rocket_description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus vel tortor non volutpat. Proin at nisl dictum, fermentum enim id, porttitor nisl. Aenean euismod nulla ac nibh dictum hendrerit. Pellentesque ultrices nisl sit amet libero consectetur, vitae facilisis turpis varius. Praesent ac efficitur augue.',
+      reserved: false
+    },
+    {
+      rocket_id:2,
+      rocket_name:'Rocket 2',
+      rocket_img: rocketImg,
+      rocket_description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus vel tortor non volutpat. Proin at nisl dictum, fermentum enim id, porttitor nisl. Aenean euismod nulla ac nibh dictum hendrerit. Pellentesque ultrices nisl sit amet libero consectetur, vitae facilisis turpis varius. Praesent ac efficitur augue.',
+      reserved: false
+    }
+  ]
 
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(FetchRockets());
-  }, []);
-
+  const data = rockets.map(rocket => <RocketCard key={rocket.rocket_id} rocket={rocket}/>)
   return (
     <div className='rocketList'>
-      {rocketsinfo.map((rocket) => <RocketCard key={rocket.rocket_id} rocket={rocket}/>)}
+      {data}
     </div>
   );
 };
