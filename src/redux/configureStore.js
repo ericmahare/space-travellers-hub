a60ce8e270a7b723df127119703f5e210b9eb7b0
ericmahare <erikmahare@gmail.com>
2022-06-13 16:56:54 +0300

    Add combine reduce file

---
+++ src/redux/configureStore.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import rocketsReducer from './rocket/rocketSlice';
+// import dragonReducer from './dragon/dragon';
+
+const reducer = combineReducers({
+  rocketsReducer
+});
+
+const store = createStore(
+  reducer,
+  applyMiddleware(logger, thunk),
+);
+
+export default store;
