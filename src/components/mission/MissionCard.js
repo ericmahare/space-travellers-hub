eaf99fb6f51760dace8032530ee17680bf9aa810
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-14 12:39:10 +0100

    Modified the MissionCard component to fetch and render list

--- src/components/mission/MissionCard.js
+++ src/components/mission/MissionCard.js
@@ -1,40 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { FetchMissions, toggleMission } from '../../redux/mission/missionSlice';
 import '../../scss/MissionCard.scss';
 import styles from './MissionCard.module.css';
 
-function MissionCard({ mission }) {
+let fetched = false;
+
+function MissionCard() {
+  const missionsState = useSelector((state) => state.missionsReducer.missions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!fetched) {
+      dispatch(FetchMissions());
+      fetched = true;
+    }
+  }, []);
 
   return (
     <table className='missionCard'>
-    <tbody>
-    <tr>
+      <tbody>
+      <tr>
         <th>Mission</th>
         <th>Description</th>
         <th>Status</th>
         <th> </th>
-    </tr>
-    
-    <tr>
-        <td className='missionName'>Thaicom</td>
-        <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus vel tortor non volutpat. Proin at nisl dictum, fermentum enim id, porttitor nisl. Aenean euismod nulla ac nibh dictum hendrerit. Pellentesque ultrices nisl sit amet libero consectetur, vitae facilisis turpis varius. Praesent ac efficitur augue.</td>
-        <td className='status'>
-        <p className={styles.notReserved}> NOT A MEMBER </p>
-        </td>
-        <td className={styles.join}>
-        <button> Join Mission </button>
-        </td>
-    </tr>
-
-    <tr>
-        <td className='missionName'>Iridium NEXT</td>
-        <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus vel tortor non volutpat. Proin at nisl dictum, fermentum enim id, porttitor nisl. Aenean euismod nulla ac nibh dictum hendrerit. Pellentesque ultrices nisl sit amet libero consectetur, vitae facilisis turpis varius. Praesent ac efficitur augue.</td>
-        <td className='status'>
-        <p className={styles.missionReserved}> ACTIVE MEMBER </p>
-        </td>
-        <td className=''>
-        <button className={styles.leave}> Leave Mission </button>
-        </td>
-    </tr>
+      </tr>
+      {missionsState.map(
+        ({
+          mission_name: missionName,
+          mission_id,
+          description,
+          reserved,
+        }) => (
+          <tr key={mission_id}>
+            <td className='missionName'>{missionName}</td>
+            <td>{description}</td>
+            <td className='status'>
+              <p
+                className={styles.missionReserved}
+              >
+                ACTIVE MEMBER
+              </p>
+            </td>
+            <td key={mission_id} className='status'>
+              <button
+                className={styles.leave}
+              >Leave Mission
+              </button>
+            </td>
+          </tr>
+        ),
+      )}
     </tbody>
     </table>
   );
