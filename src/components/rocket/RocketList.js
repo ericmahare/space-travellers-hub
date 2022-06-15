8f766086a611d2426525bf7a5d3622f2fb9f7ba1
ericmahare <erikmahare@gmail.com>
2022-06-15 23:26:11 +0300

    Remove data fetching action

--- src/components/rocket/RocketList.js
+++ src/components/rocket/RocketList.js
@@ -1,18 +1,10 @@
-import React, { useEffect } from 'react';
 import RocketCard from './RocketCard';
 import '../../scss/RocketList.scss';
-import rocketImg from '../../images/rocket.jpg';
-import { FetchRockets } from '../../redux/rocket/rocketSlice';
-import { useDispatch, useSelector } from 'react-redux';
+
+import { useSelector } from 'react-redux';
 const Rockets = () => {
   const rockets = useSelector((state) => state.rocketsReducer);
-  const dispatch = useDispatch();
-  
-  // fetch api data
-  useEffect(() => {
-    dispatch(FetchRockets())
-  }, [])
-  
+ 
   const data = rockets.map(rocket => <RocketCard key={rocket.rocket_id} rocket={rocket}/>)
   return (
     <div className='rocketList'>
