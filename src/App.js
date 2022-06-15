3fc9852b936782a1984dd50db9329d1c0ded3fee
ericmahare <erikmahare@gmail.com>
2022-06-15 23:24:53 +0300

    Fetch rocket data from the API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Rocket from './pages/Rocket';
 import Mission from './pages/Mission';
 import Profile from './pages/Profile';
-
+import { FetchRockets } from './redux/rocket/rocketSlice';
+import { useDispatch } from 'react-redux';
 function App() {
+  const dispatch = useDispatch();
+   
+  // fetch api data
+  useEffect(() => {
+    dispatch(FetchRockets())
+  }, [])
   return (
     <>
       <Navbar />
